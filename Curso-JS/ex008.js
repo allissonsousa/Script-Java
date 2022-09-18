399e391a306dea508e45c5a1335d38521b1df375
Allison de Paulo Sousa Avila <111160403+allissonsousa@users.noreply.github.com>
2022-09-18 16:18:28 -0300

    novo exercicio

---
+++ Curso-JS/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A velocidade so seu carro é ${vel}km/h`)
+if (vel > 60) { /* condição simples */
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Diriga sempre com cinto de segurança')
